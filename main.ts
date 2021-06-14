b08fca18da632823ca9c7474169dd2c76b3c7f75
guada-123 <83685388+guada-123@users.noreply.github.com>
2021-06-13 19:24:43 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(input.temperature())
+})
+input.onButtonPressed(Button.B, function () {
+    let lectura_analógica_pin = 0
+    basic.showNumber(lectura_analógica_pin)
+})
+basic.forever(function () {
+	
+})
